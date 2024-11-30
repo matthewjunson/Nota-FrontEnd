74f825eab84f2a7a2360f07894e0525e0ed5abd6
Rafael Posadas <rcposadas@pm.me>
2024-11-30 05:34:20 -0500

    Started making UI for sticky notes
    This commit includes updates to some of the barebones components and update the .gitignore.

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,9 +1,25 @@
 // This component will mainly display the miniaturized sticky notes shown on the NotesBoard
 // This will also handle action when mouse clicks on and hovers over the note
 
-function NoteItem() {
+/* eslint-disable react/prop-types */
+
+function NoteItem(props) {
     return (
-        <></>
+        <>
+            <div className="noteItemContainer">
+                <div className="title">{props.Title}</div>
+                <div className="body">{props.Body}</div>
+                <div className="footer">
+                    <div className="category">{props.Category}</div>
+                    <div className="noteButtonMenu">
+                        <button id='edit' className='noteButton' onClick={props.onClick}/>
+                        <button id='color' className='noteButton' onClick={props.onClick}/>
+                        <button id='reminder' className='noteButton' onClick={props.onClick}/>
+                        <button id='menu' className='noteButton' onClick={props.onClick}/>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/NotesBoard.jsx
+++ src/components/NotesBoard.jsx
@@ -1,10 +1,18 @@
 // This component will encapsulate the actual sticky notes. Similar to the BookLibrary exercise.
 
+/* eslint-disable react/prop-types */
+import "./NotesBoard.css"
 import NoteItem from "./NoteItem.jsx";
-
 function NotesBoard() {
     return (
-        <></>
+        <>
+
+            <NoteItem
+                Title={"COMP229 WEB DEVELOPMENT"}
+                Body={"Hello World!"}
+                Category={"Courses"}
+            />
+        </>
     )
 }
 
