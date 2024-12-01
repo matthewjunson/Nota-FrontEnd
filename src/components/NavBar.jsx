a73882ac4cb26162f1039586eefe86d287945d4a
Prem-poriya <pporiya@my.centennialcollege.ca>
2024-11-30 22:05:59 -0500

    NavBar
    this is nav bar jsx and navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,25 @@
 // This is separate from the sidebar mainly to make it easier (hopefully) when configuring the CSS
 // regarding flex containers and items and stuff.
 
+import React from "react";
+import "./NavBar.css";
+
 function NavBar() {
     return (
-        <></>
-    )
+        <div className="navbar">
+            {/* Logo Section */}
+            <div className="navbar-logo">
+                <img src="./assets/icons8-note-keek-16.png" alt="Logo" />
+                <h1>Notes App</h1>
+            </div>
+
+            {/* Search Bar Section */}
+            <div className="navbar-search">
+                <input type="text" placeholder="Search notes..." />
+                <button>Search</button>
+            </div>
+        </div>
+    );
 }
 
 export default NavBar;
